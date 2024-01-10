62b10b762970663a008d9f3680d69f4c3cddae49
gabribn <mnfgabriel@gmail.com>
2024-01-10 13:23:45 -0300

    OrderItem, many to many association

---
+++ src/main/java/com/gabrielreis/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.gabrielreis.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gabrielreis.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+
+}
