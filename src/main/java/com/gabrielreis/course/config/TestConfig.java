86ab290d38d0298ad39549f21c10cff4327d1864
gabribn <mnfgabriel@gmail.com>
2024-01-11 12:50:56 -0300

    Payment and one to one association

--- src/main/java/com/gabrielreis/course/config/TestConfig.java
+++ src/main/java/com/gabrielreis/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.gabrielreis.course.entities.Category;
 import com.gabrielreis.course.entities.Order;
 import com.gabrielreis.course.entities.OrderItem;
+import com.gabrielreis.course.entities.Payment;
 import com.gabrielreis.course.entities.Product;
 import com.gabrielreis.course.entities.User;
 import com.gabrielreis.course.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T22:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 	
 	
