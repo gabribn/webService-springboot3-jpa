161264386af7db59ac12693fe8c3a1b191254161
gabribn <mnfgabriel@gmail.com>
2024-01-12 10:43:29 -0300

    User Uptade

--- src/main/java/com/gabrielreis/course/services/UserService.java
+++ src/main/java/com/gabrielreis/course/services/UserService.java
@@ -4,8 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.gabrielreis.course.entities.User;
@@ -32,4 +30,15 @@ public class UserService {
 		repository.deleteById(id);
 	}
 
+	public User update(Long id, User obj) {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+	}
+	
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
